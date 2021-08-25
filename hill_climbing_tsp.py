0e809400bd1dab56183831034d632c1ed079bdf3
Luana Porciuncula <lpb@cin.ufpe.br>
2021-08-25 11:31:56 -0300

    fix issue in swap

--- hill_climbing_tsp.py
+++ hill_climbing_tsp.py
@@ -4,6 +4,38 @@ import copy
 # import matplotlib.pyplot as plt
 
 
+def main():
+    coord_nodes = []
+
+    # obtendo nós das coordenadas do dataset
+    with open("database/wi29.tsp", "r") as tsp_file:
+        tsp_file_lines = tsp_file.read().splitlines()
+
+        for line in tsp_file_lines[7:-1]:
+            line_elems = line.split(" ")
+            coord_nodes.append([float(line_elems[1]), float(line_elems[2])])
+
+    best_solution = coord_nodes
+    best_dist_solution = float('inf')
+
+    for _ in range(10):
+        random_config = generate_random_config(coord_nodes)
+        distance = get_travelled_dist(random_config)
+
+        solution, dist_solution, iter_num = hill_climbing(distance, random_config, 1)
+
+        # print(solution)
+        print("distance: ", dist_solution, ", number of iter: ", iter_num)
+
+        if (dist_solution < best_dist_solution):
+            best_solution = solution
+            best_dist_solution = dist_solution
+            print("NEW BEST", best_dist_solution)
+        print("------------------------------")
+    
+    print("BEST ITERATION: ",best_solution, best_dist_solution)
+
+
 def generate_random_config(coord_nodes):
     random_config = copy.deepcopy(coord_nodes)
     random.shuffle(random_config)
@@ -11,7 +43,6 @@ def generate_random_config(coord_nodes):
     
 
 def get_travelled_dist(solution):
-    # sourcery skip: inline-immediately-returned-variable, sum-comprehension
     distance = 0
     for i in range(len(solution) - 1):
         distance += math.dist(solution[i], solution[i + 1])
@@ -20,19 +51,20 @@ def get_travelled_dist(solution):
 
 
 def generate_childs_config(solution):
-    n_swap_pairs = 5
+    n_swap_pairs = 3
     childs = []
 
-    for _ in range(100):
+    for _ in range(1000):
         child = copy.deepcopy(solution)
 
         swaps = random.sample(range(len(child)), n_swap_pairs*2)
         random.shuffle(swaps)
         
         for i in range(n_swap_pairs):
-            child[i], child[i+n_swap_pairs] = child[i+n_swap_pairs], child[i]
+            child[swaps[i]], child[swaps[i+n_swap_pairs]] = child[swaps[i+n_swap_pairs]], child[swaps[i]]
 
         childs.append(child)
+
     return childs
 
 
@@ -49,45 +81,15 @@ def get_best_child(childs):
     return curr_best_child_dist, curr_best_child
 
 
-def hill_climbing(curr_best_dist, curr_solution):
+def hill_climbing(curr_best_dist, curr_solution, iter_num):
     childs = generate_childs_config(curr_solution)
     curr_best_child_dist, curr_best_child = get_best_child(childs)
-    
     if curr_best_child_dist < curr_best_dist:
-        return hill_climbing(curr_best_child_dist, curr_best_child)
+        return hill_climbing(curr_best_child_dist, curr_best_child, iter_num+1)
     else:
         # print("local minimum found:", curr_solution)
         # print("local minimum dist:", curr_best_dist)
-        return [curr_solution, curr_best_dist]
-
-
-def main():
-    coord_nodes = []
-
-    with open("database/wi29.tsp", "r") as tsp_file:
-        tsp_file_lines = tsp_file.read().splitlines()
-
-        for line in tsp_file_lines[7:-1]:
-            line_elems = line.split(" ")
-            coord_nodes.append([float(line_elems[1]), float(line_elems[2])])
-
-    best_solution = coord_nodes
-    best_dist_solution = float('inf')
-
-    for _ in range(10):
-        random_config = generate_random_config(coord_nodes)
-        distance = get_travelled_dist(random_config)
-
-        solution = hill_climbing(distance, random_config)
-        print(solution[0], solution[1])
-
-        if (solution[1] < best_dist_solution):
-            best_solution = solution[0]
-            best_dist_solution = solution[1]
-            print("NEW BEST", best_dist_solution)
-        print("------------------------------")
-    
-    print("BEST ITERATION: ",best_solution, best_dist_solution)
+        return [curr_solution, curr_best_dist, iter_num]
     
 
 if __name__ == "__main__":
